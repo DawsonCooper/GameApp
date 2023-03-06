0aad02aed61d7c0017d820cb069944ad7384beaa
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-03-06 13:18:22 -0600

    Small setup testing api ran into cors error will need to look for a workaround

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Axios from 'axios';
 
 function App() {
+  const getGames = () => {
+    Axios({
+      url: "https://api.igdb.com/v4/games",
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Client-ID': '4z6o2oyyofxuxlyrr3seabytxji1f5',
+          'Authorization': '52d0b18je4h7nupv6dzjci2uv5nzes',
+      },
+      data: "fields age_ratings;"
+    })
+      .then(response => {
+          console.log(response.data);
+      })
+      .catch(err => {
+          console.error(err);
+      });
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getGames}>Get Creds</button>
       </header>
     </div>
   );
---
+++ src/components/cred.js
@@ -0,0 +1,8 @@
+// Creditals for the igdb api calls
+
+const cred = {
+    'access_token': '52d0b18je4h7nupv6dzjci2uv5nzes',
+    'client_id': '4z6o2oyyofxuxlyrr3seabytxji1f5',
+    'secret': 'uqisltwqj0p4dp55mkj8t7aovtfprs',
+}
+export default cred
